e4c15c1be0e3d682c91d2e0b5b8eddcb8193be44
Zubair-hussain <detroonshah@gmail.com>
2026-04-30 03:05:43 +0500

    feat(backend): add UserRegisterSerializer

--- xis_backend/images/serializers.py
+++ xis_backend/images/serializers.py
@@ -38,3 +38,21 @@ class ImageUploadSerializer(serializers.ModelSerializer):
                 "Unsupported file type. Use JPG, PNG, WebP, or GIF."
             )
         return file
+
+
+from django.contrib.auth.models import User
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'email']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password'],
+            email=validated_data.get('email', '')
+        )
+        return user
